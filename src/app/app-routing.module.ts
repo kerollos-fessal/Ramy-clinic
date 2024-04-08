5e8e6c7c2c0ea859037152d42ecc2e4e5623cd14
Kerollos Fessal <kerofessal77@gmail.com>
2024-04-09 00:54:03 +0200

    project routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AppointmentsComponent } from './pages/appointments/appointments.component';
+import { ReviewsComponent } from './pages/reviews/reviews.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'appointments', component: AppointmentsComponent },
+  { path: 'reviews', component: ReviewsComponent },
+
+
+
+
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ReviewsComponent } from './pages/reviews/reviews.component';
+import { AppointmentsComponent } from './pages/appointments/appointments.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ReviewsComponent,
+    AppointmentsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
